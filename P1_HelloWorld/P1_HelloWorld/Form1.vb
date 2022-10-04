db7bd0882bacc959b8a954d904b3f3e036efe84d
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2022-10-04 13:53:48 +0300

    Första projektet klart

---
+++ P1_HelloWorld/P1_HelloWorld/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub tryckHarKnapp_Click(sender As Object, e As EventArgs) Handles tryckHarKnapp.Click
+        MsgBox("Hej Kjell, kul att du lyckades trycka på knappen")
+    End Sub
+End Class
