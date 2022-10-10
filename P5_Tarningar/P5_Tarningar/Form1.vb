92a507c3ffdb994c546ef612be6245fa4b2c8dbd
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2022-10-10 10:25:13 +0300

    Undvik onödig väntan på att tärningarna ska stanna

--- P5_Tarningar/P5_Tarningar/Form1.vb
+++ P5_Tarningar/P5_Tarningar/Form1.vb
@@ -2,8 +2,8 @@
     Private Sub btnSla_Click(sender As Object, e As EventArgs) Handles btnSla.Click
         Dim varde As Integer
         Dim antalRundor1 = Int(10 * Rnd()) + 11
-        Dim antalRundor2 = Int(10 * Rnd()) + 11
-        Dim antalRundor3 = Int(10 * Rnd()) + 11
+        Dim antalRundor2 = Int(20 * Rnd()) + 11
+        Dim antalRundor3 = Int(30 * Rnd()) + 11
 
         ' Hämta aktuell cursor och spara undan värdet
         Dim c As Cursor = Cursor
@@ -14,7 +14,7 @@
         btnSla.Enabled = False
 
         ' Animera tärningarna
-        For i = 1 To 20
+        For i = 1 To 40
             If i < antalRundor1 Then
                 varde = Int(6 * Rnd())
                 picTarning1.Image = imlTarningar.Images(varde)
@@ -29,6 +29,12 @@
                 varde = Int(6 * Rnd())
                 picTarning3.Image = imlTarningar.Images(varde)
             End If
+
+            ' Hoppa ur loopen om räknaren är större än antalet rundor för alla tärningarna
+            If i >= antalRundor1 AndAlso i >= antalRundor2 AndAlso i >= antalRundor3 Then
+                Exit For
+            End If
+
             ' Gör en DoEvents-loop för att se att tärningarna rör sig
             For j = 0 To 1000000
                 Application.DoEvents()
