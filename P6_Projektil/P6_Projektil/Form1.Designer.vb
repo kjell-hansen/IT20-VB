1d43c5676f2abc080d264fe8e5066c932afe9151
Kjell Hansen (S1-KJHA-2905) <kjell.hansen@gymnasium.ax>
2022-10-13 09:37:41 +0300

    Ritar ut projektilbanan och ett mål

---
+++ P6_Projektil/P6_Projektil/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtVinkel = New System.Windows.Forms.TextBox()
+        Me.txtHastighet = New System.Windows.Forms.TextBox()
+        Me.btnRita = New System.Windows.Forms.Button()
+        Me.btnRensa = New System.Windows.Forms.Button()
+        Me.picWorld = New System.Windows.Forms.PictureBox()
+        CType(Me.picWorld, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(49, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Vinkel"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(47, 121)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Hastighet"
+        '
+        'txtVinkel
+        '
+        Me.txtVinkel.Location = New System.Drawing.Point(132, 69)
+        Me.txtVinkel.MaxLength = 2
+        Me.txtVinkel.Name = "txtVinkel"
+        Me.txtVinkel.Size = New System.Drawing.Size(62, 27)
+        Me.txtVinkel.TabIndex = 2
+        Me.txtVinkel.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtHastighet
+        '
+        Me.txtHastighet.Location = New System.Drawing.Point(132, 118)
+        Me.txtHastighet.MaxLength = 3
+        Me.txtHastighet.Name = "txtHastighet"
+        Me.txtHastighet.Size = New System.Drawing.Size(62, 27)
+        Me.txtHastighet.TabIndex = 2
+        Me.txtHastighet.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnRita
+        '
+        Me.btnRita.Location = New System.Drawing.Point(128, 181)
+        Me.btnRita.Name = "btnRita"
+        Me.btnRita.Size = New System.Drawing.Size(71, 36)
+        Me.btnRita.TabIndex = 3
+        Me.btnRita.Text = "Rita!"
+        Me.btnRita.UseVisualStyleBackColor = True
+        '
+        'btnRensa
+        '
+        Me.btnRensa.Location = New System.Drawing.Point(128, 235)
+        Me.btnRensa.Name = "btnRensa"
+        Me.btnRensa.Size = New System.Drawing.Size(71, 36)
+        Me.btnRensa.TabIndex = 3
+        Me.btnRensa.Text = "Rensa"
+        Me.btnRensa.UseVisualStyleBackColor = True
+        '
+        'picWorld
+        '
+        Me.picWorld.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.picWorld.BackColor = System.Drawing.Color.White
+        Me.picWorld.Location = New System.Drawing.Point(291, 6)
+        Me.picWorld.Name = "picWorld"
+        Me.picWorld.Size = New System.Drawing.Size(497, 430)
+        Me.picWorld.TabIndex = 4
+        Me.picWorld.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 442)
+        Me.Controls.Add(Me.picWorld)
+        Me.Controls.Add(Me.btnRensa)
+        Me.Controls.Add(Me.btnRita)
+        Me.Controls.Add(Me.txtHastighet)
+        Me.Controls.Add(Me.txtVinkel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1100, 750)
+        Me.MinimumSize = New System.Drawing.Size(630, 345)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picWorld, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtVinkel As TextBox
+    Friend WithEvents txtHastighet As TextBox
+    Friend WithEvents btnRita As Button
+    Friend WithEvents btnRensa As Button
+    Friend WithEvents picWorld As PictureBox
+End Class
---
+++ P6_Projektil/P6_Projektil/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+    Private target As Point
+
+    Private Sub txtVinkel_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtVinkel.KeyPress, txtHastighet.KeyPress
+        If (e.KeyChar < "0"c OrElse e.KeyChar > "9"c) AndAlso e.KeyChar <> vbBack Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtVinkel_Leave(sender As Object, e As EventArgs) Handles txtVinkel.Leave
+        If Val(txtVinkel.Text) > 89 OrElse Val(txtVinkel.Text) < 1 Then
+            txtVinkel.BackColor = Color.Pink
+        Else
+            txtVinkel.BackColor = SystemColors.Window
+        End If
+    End Sub
+
+    Private Sub btnRita_Click(sender As Object, e As EventArgs) Handles btnRita.Click
+        Dim x, y, tid, hastighet, vinkel As Single
+        Dim punkt As System.Drawing.Graphics
+        Dim penna As New System.Drawing.Pen(Brushes.Black, 4)
+        Dim oldCursor As Cursor = Cursor.Current
+
+        Cursor = Cursors.WaitCursor
+        btnRita.Enabled = False
+        btnRensa.Enabled = False
+
+        hastighet = Val(txtHastighet.Text)
+        vinkel = Val(txtVinkel.Text)
+
+        punkt = picWorld.CreateGraphics
+
+        Do
+            tid = tid + 0.25
+            x = hastighet * Math.Cos(vinkel * Math.PI / 180) * tid
+            y = picWorld.Height - (hastighet * Math.Sin(vinkel * Math.PI / 180) * tid - 9.82 * tid * tid / 2)
+
+            punkt.DrawEllipse(penna, x, y, 2, 2)
+        Loop While (x < picWorld.Width)
+
+        btnRensa.Enabled = True
+        btnRita.Enabled = True
+        Cursor = oldCursor
+    End Sub
+
+    Private Sub btnRensa_Click(sender As Object, e As EventArgs) Handles btnRensa.Click
+        picWorld.CreateGraphics.Clear(SystemColors.Window)
+    End Sub
+
+    Private Function slumpaMal() As Point
+        Dim mal As Point
+
+        mal.X = picWorld.Width * Rnd()
+        mal.Y = picWorld.Height * Rnd()
+
+        Return mal
+
+    End Function
+    Private Sub ritaMal()
+        Dim punkt As System.Drawing.Graphics
+        Dim penna As New System.Drawing.Pen(Brushes.Red, 4)
+
+        punkt = picWorld.CreateGraphics
+        punkt.DrawEllipse(penna, target.X, target.Y, 4, 4)
+
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        ritaMal()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        target = slumpaMal()
+    End Sub
+End Class
